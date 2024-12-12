8764cdd12ff6966cdfa7558e2b29ab82f99ce49b
Alberto García <albertogd96@gmail.com>
2024-12-12 21:27:24 +0100

    v1.1
    Multi hilos

--- scanPorts.sh
+++ scanPorts.sh
@@ -8,5 +8,5 @@ function ctrl_c(){
 trap ctrl_c INT
 
 for port in $(seq 1 65535); do 
-  (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "$port --OPEN" 
-done 
+  (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "$port --OPEN" &
+done; wait 
